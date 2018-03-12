44d4b4ba7263f48f9fc14829cf856cce0eca0d6d
EWeatherill <ew7@sanger.ac.uk>
2018-03-12 16:46:56 +0000

    Struggling to get GitPython to work.

--- server/entrypoint.py
+++ server/entrypoint.py
@@ -20,10 +20,21 @@ Entry point for the handling of GitHub pushes.
 import json
 import os
 import tempfile
+import bottle
 from git import Repo, Remote, PushInfo
 from bottle import Bottle, LocalRequest, LocalResponse, response, request
 
 from server.wsgi_server import WsgiServerController
+
+
+def load_config(path: os.path):
+    """
+
+    :return:
+    """
+    with open(path) as file:
+        json_config = json.load(file)
+    return json_config
 
 
 # The following functions are route handling for a local Bottle application.
@@ -33,34 +44,31 @@ def handle_github_push(req: LocalRequest, res: LocalResponse):
     """
 
     # Find which Github repos should be synced.
-    config_file = os.path.join(os.path.dirname(__file__), "../repositories_to_sync.json")
-    with open(config_file) as file:
-        json_config = json.load(file)
-    github_repos = json_config["github_repos"]
+    config = load_config(os.path.join(os.path.dirname(__file__), "../repositories_to_sync.json"))
+    github_repos = config["github_repos"]
 
     # Check if message relates to a repo that should be synced.
-    message = req.json()
+    message = json.loads(req.json)
     repo_name = message["repository"]["name"]
     if repo_name not in github_repos:
-        return res.status_code(204)
+        bottle.abort(404, "Repo not under GitLib")
 
     # Get the repo URL
-    github_repo_url = message["repository"]["html_url"]
+    github_repo_url = message["repository"]["url"]
 
     # Create a temp directory to clone the repo to:
-    with tempfile.TemporaryDirectory as temp_dir:
+    with tempfile.TemporaryDirectory() as temp_dir:
         local_repo = Repo.clone_from(github_repo_url, temp_dir)
 
         # Push to gitlab
-        gitlab_url = json_config["gitlab_url"] + repo_name
+        gitlab_url = config["gitlab_url"] + repo_name
         push_info = push_github_repo_to_gitlab(local_repo, gitlab_url)
-
         # Todo - verify push_info.  See: http://gitpython.readthedocs.io/en/stable/reference.html#git.remote.PushInfo
 
-    return res.status_code(200)
+    return res
 
 
-def push_github_repo_to_gitlab(github_repo: Repo, gitlab_url) -> [PushInfo]:
+def push_github_repo_to_gitlab(github_repo: Repo, gitlab_url: str) -> [PushInfo]:
     """
 
     :param github_repo:
@@ -69,8 +77,8 @@ def push_github_repo_to_gitlab(github_repo: Repo, gitlab_url) -> [PushInfo]:
              See: http://gitpython.readthedocs.io/en/stable/reference.html#git.remote.Remote.push
     """
 
-    gitlab_repo = Remote.add(github_repo, 'gitlab', gitlab_url)
-    return gitlab_repo.push()
+    gitlab_remote = Remote.create(github_repo, 'gitlab', gitlab_url)
+    return gitlab_remote.push()
 
 
 def create_server() -> Bottle:
--- server/tests/test_entrypoint.py
+++ server/tests/test_entrypoint.py
@@ -1,7 +1,9 @@
 import unittest
+from unittest.mock import MagicMock
 import os
 import json
 import requests
+import server
 
 from server.entrypoint import create_server
 from server.wsgi_server import WsgiServerController
@@ -10,24 +12,33 @@ from server.wsgi_server import WsgiServerController
 class TestEntrypoint(unittest.TestCase):
 
     def setUp(self):
-        server = create_server()
-        self.server_controller = WsgiServerController(server)
+        print("Set up")  # debug
+        test_server = create_server()
+        self.server_controller = WsgiServerController(test_server)
         self.server_controller.start()
+
+        # Load test config to point to local test repos
+        server.entrypoint.load_config = MagicMock(return_value={
+            "github_repos": ["the_repo"],
+            "gitlab_url": os.path.join(os.path.dirname(__file__), "gitlab_test_repo/")
+        })
 
 
     def tearDown(self):
+        print("Tear down") # debug
         self.server_controller.stop()
 
     def test_response(self):
+        print("Starting test_response")  # debug
 
         data = json.dumps({
             "repository": {
                 "name": "the_repo",
-                "html_url": os.path.dirname(__file__) + "github_test_repo/"
+                "url": os.path.join(os.path.dirname(__file__), "github_test_repo/the_repo")
             }
         })
 
-        response = requests.post('file://', data)
+        response = requests.post(self.server_controller.url, json=data, headers={'content-type': 'application/json'})
         self.assertEqual(200, response.status_code)
 
 
