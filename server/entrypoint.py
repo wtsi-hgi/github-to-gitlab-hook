300502b555562080ea93746a45314bb22f1469f6
EWeatherill <ew7@sanger.ac.uk>
2018-03-13 18:34:11 +0000

    Test still doesn't work but I think it's close.  Added creation of local git remotes to the tests and worked on sync logic.

--- server/entrypoint.py
+++ server/entrypoint.py
@@ -29,8 +29,8 @@ from server.wsgi_server import WsgiServerController
 
 def load_config(path: os.path):
     """
+    Loads a json file used for config.  Handy to have the function separated for testing purposes.
 
-    :return:
     """
     with open(path) as file:
         json_config = json.load(file)
@@ -38,9 +38,14 @@ def load_config(path: os.path):
 
 
 # The following functions are route handling for a local Bottle application.
-def handle_github_push(req: LocalRequest, res: LocalResponse):
+def handle_github_push_request(req: LocalRequest, res: LocalResponse):
     """
+    This checks the contents of the repositories_to_sync.json config file against the HTML request contents,
+    specifically the name of the repo in the request, and if present, call the function to handle between github
+    and gitlab.
 
+    :param req: the Bottle.request which is the current request being handled by the server.
+    :param res: the Bottle.response which is the current response being handled by the server.
     """
 
     # Find which Github repos should be synced.
@@ -53,31 +58,33 @@ def handle_github_push(req: LocalRequest, res: LocalResponse):
     if repo_name not in github_repos:
         bottle.abort(404, "Repo not under GitLib")
 
-    # Get the repo URL
-    github_repo_url = message["repository"]["url"]
-
-    # Create a temp directory to clone the repo to:
-    with tempfile.TemporaryDirectory() as temp_dir:
-        local_repo = Repo.clone_from(github_repo_url, temp_dir)
-
-        # Push to gitlab
-        gitlab_url = config["gitlab_url"] + repo_name
-        push_info = push_github_repo_to_gitlab(local_repo, gitlab_url)
-        # Todo - verify push_info.  See: http://gitpython.readthedocs.io/en/stable/reference.html#git.remote.PushInfo
+    # Do the hard work of syncing.
+    sync_github_repo_to_gitlab(repo_name, message["repository"]["url"], config["gitlab_url"])
 
     return res
 
 
-def push_github_repo_to_gitlab(github_repo: Repo, gitlab_url: str) -> [PushInfo]:
+def sync_github_repo_to_gitlab(repo_name: str, github_repo_url, gitlab_domain_url) -> [PushInfo]:
     """
+    Create a temporary clone of the GitHub repo and set a remote to push to the associated GitLab repo.
 
-    :param github_repo:
-    :param gitlab_url:
+    :param repo_name: the name of the repo from GitHub which should match an associated GitLab repo.
+    :param github_repo_url: the location of the GitHub repo.
+    :param gitlab_domain_url: the location of the GitLab domain where all the repos are.
     :return: Iterable list of push information.
              See: http://gitpython.readthedocs.io/en/stable/reference.html#git.remote.Remote.push
     """
 
-    gitlab_remote = Remote.create(github_repo, 'gitlab', gitlab_url)
+    # Todo: get this to work with GitPython.  Failing that maybe use subprocess.
+
+    # Create a temp directory to clone the repo to:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        local_repo = Repo.clone_from(github_repo_url, temp_dir)
+        gitlab_remote = local_repo.create_remote('gitlab', gitlab_domain_url + repo_name)
+        assert gitlab_remote.exists()
+        gitlab_remote.fetch()
+
+        local_repo.commit()
     return gitlab_remote.push()
 
 
@@ -89,7 +96,7 @@ def create_server() -> Bottle:
     """
 
     app = Bottle()
-    app.route(path="/", method="POST", callback=lambda: handle_github_push(request, response))
+    app.route(path="/", method="POST", callback=lambda: handle_github_push_request(request, response))
 
     return app
 
--- server/tests/test_entrypoint.py
+++ server/tests/test_entrypoint.py
@@ -4,6 +4,8 @@ import os
 import json
 import requests
 import server
+from tempfile import TemporaryDirectory
+from git import Repo
 
 from server.entrypoint import create_server
 from server.wsgi_server import WsgiServerController
@@ -12,34 +14,76 @@ from server.wsgi_server import WsgiServerController
 class TestEntrypoint(unittest.TestCase):
 
     def setUp(self):
-        print("Set up")  # debug
+
+        # Create the server and a controller to help start up and shutdown processes.
         test_server = create_server()
         self.server_controller = WsgiServerController(test_server)
         self.server_controller.start()
 
-        # Load test config to point to local test repos
-        server.entrypoint.load_config = MagicMock(return_value={
-            "github_repos": ["the_repo"],
-            "gitlab_url": os.path.join(os.path.dirname(__file__), "gitlab_test_repo/")
-        })
+        # Set up two git remotes to act as test GitHub and GitLab.
+        self.github_temp_dir = TemporaryDirectory()
+        self.github_remote = Repo.init(self.github_temp_dir.name, bare=True)
+        assert self.github_remote.bare
 
+        self.gitlab_temp_dir = TemporaryDirectory()
+        self.gitlab_remote = Repo.init(self.gitlab_temp_dir.name, bare=True)
+        assert self.gitlab_remote.bare
 
     def tearDown(self):
-        print("Tear down") # debug
         self.server_controller.stop()
+        self.github_temp_dir.cleanup()
+        self.gitlab_temp_dir.cleanup()
 
-    def test_response(self):
-        print("Starting test_response")  # debug
+    def create_clone_repo_with_readme(self, url_of_repo_to_clone: str, location_of_clone: str):
+        """
+        This creates a clone of a remote so it can be used to pull and push easily.  It adds a README.txt so
+        a change of state can be committed and pushed to the remote.
 
-        data = json.dumps({
-            "repository": {
-                "name": "the_repo",
-                "url": os.path.join(os.path.dirname(__file__), "github_test_repo/the_repo")
-            }
+        :param url_of_repo_to_clone: can be a local git repo
+        :param location_of_clone: can also be a local directory
+        :return: a Bottle repository object for the cloned repo.
+        """
+        clone = Repo.clone_from(url_of_repo_to_clone, location_of_clone)
+        assert clone.remotes['origin'].exists()
+        clone.remotes['origin'].fetch()
+
+        # Create a file in the cloned repo.
+        new_file_path = os.path.join(location_of_clone, "README.txt")
+        with open(new_file_path, 'w') as file:
+            file.write("Hello world")
+
+        # Add and commit the new file.
+        clone.index.add([new_file_path])
+        clone.index.commit("Adding README.txt")
+        clone.commit()
+        clone.remotes['origin'].push()
+
+    def test_sync_github_to_gitlab(self):
+        """
+        Testing for a successful HTML response and that the contents of the GitHub test repo reached the
+        GitLab test repo.
+
+        :return:
+        """
+
+        with TemporaryDirectory() as temp_dir:
+            # Intitalise a temp clone and push a readme to the GitHub test remote.
+            self.create_clone_repo_with_readme(self.github_temp_dir.name, temp_dir)
+
+        # Load test config to point to local test repos
+        server.entrypoint.load_config = MagicMock(return_value={
+            "github_repos": [self.github_temp_dir.name],
+            "gitlab_url": self.gitlab_temp_dir.name
         })
 
-        response = requests.post(self.server_controller.url, json=data, headers={'content-type': 'application/json'})
+        # JSON to use in the HTML request to the server.
+        data = json.dumps({"repository": {"name": self.github_temp_dir.name, "url": self.gitlab_temp_dir.name}})
+
+        # Capture and test the response.
+        response = requests.post(self.server_controller.url, json=data,
+                                 headers={'content-type': 'application/json'})
         self.assertEqual(200, response.status_code)
+        # Todo: check that readme has successful passed from GitHub test repo to GitLab test repo.
 
 
 if __name__ == '__main__':
